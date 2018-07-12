9b4a6d152cbd2e45f77ddb1d5fa214ad9579c7af
Corey Sutphin <sutphincj@vcu.edu>
2018-07-12 00:48:47 -0600

    Set up structure of public folder

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const port = process.env.port || 3000;
 
 const app = express();
 
 app.set("view engine", "pug");
+app.set('views', path.join(__dirname, '/public/views'));
+
+app.use(express.static(__dirname + '/public')) // Include CSS values
+app.use(bodyParser.json()); // For parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
 // Replace with your db username and password
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds129831.mlab.com:29831/reddit-clone-cs', { useNewUrlParser: true },
@@ -14,9 +20,6 @@ mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds129831.mlab.com:29831/reddit
   if (err) throw err;
   console.log("Successfully connected to MongoDB");
 });
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.render("subreddit");
