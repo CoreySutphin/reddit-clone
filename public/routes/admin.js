b6353d8bd15e54a8832ee2c4ecfb609bb48ebce2
Alafran <woodside.joseph@gmail.com>
2018-08-08 12:32:32 -0400

    Added full functionality for /admin/subreddits

--- public/routes/admin.js
+++ public/routes/admin.js
@@ -7,6 +7,7 @@ const { check, validationResult} = require('express-validator/check');
 //models
 let Post = require(path.join(appRoot + '/models/PostSchema'));
 let User = require(path.join(appRoot + '/models/UserSchema'));
+let Subreddits = require(path.join(appRoot + '/models/SubredditSchema'));
 
 router.use(express.static(appRoot + '/public')) // Include static files
 
@@ -149,6 +150,132 @@ router.delete('/delete_user/:id', (req,res) => {
       res.send('Success');
     }
   })
+});
+
+/*
+  /admin/subreddits routes
+*/
+
+//Route for displaying admin subreddits page
+router.get('/subreddits', (req, res) => {
+  Subreddits.find({}, (err, allsubreddits) => {
+    res.render('admin_subreddits', {
+      title: 'Subreddits',
+      subreddits: allsubreddits
+    })
+  });
+});
+
+//Route for editing the subreddit
+router.get('/edit_sub/:id', (req, res) => {
+  let subID = req.params.id;
+  Subreddits.findOne({ _id: subID }, (err, subFromDB) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('admin_edit_sub', {
+        title: 'Edit Subreddit',
+        subData: subFromDB
+      });
+    }
+  });
+});
+
+//Route for editing the subreddit
+router.post('/edit_sub/:id',[
+  check('title').not().isEmpty().withMessage('Title is required'),
+  check('description').not().isEmpty().withMessage('Description is required'),
+  check('sidebar').not().isEmpty().withMessage('Sidebar is required')
+], (req, res) => {
+  let subID = req.params.id;
+
+  //Check for any errors in the validation
+  const errors = validationResult(req).array();
+  //if there are errors the template gets rerendered with the error messages
+  if(errors.length !== 0) {
+    Subreddits.findOne({_id: subID}, (err, subFromDB) => {
+      if(err) {
+        console.log(err);
+      } else {
+        res.render('admin_edit_sub', {
+          title: 'Edit subreddit',
+          subData: subFromDB,
+          errors: errors
+        });
+      }
+    });
+    //else No errors with input validation
+  } else {
+    //All of the fields from the form to be updated
+    let title = req.body.title;
+    let description = req.body.description;
+    let sidebar = req.body.sidebar;
+
+    //The updated fields to be passed to the DB
+    let update = {
+      title:title,
+      description:description,
+      sidebar:sidebar
+    }
+
+    //Updates the subreddit and redirects to subreddits page if no errors
+    Subreddits.findOneAndUpdate({_id: subID}, update, {new:true}, (err, updatedSub) => {
+      if(err) {
+        console.log(err);
+      } else {
+        res.redirect('/admin/subreddits');
+      }
+    });
+  }
+});
+
+//Route to handle the deleting a subreddit
+//First finds all users subscribed to that sub and removes the subscription
+//Then deletes the subreddit
+router.delete('/delete_sub/:id', (req, res) => {
+  //ID of sub to be deleted
+  let query = {_id: req.params.id};
+
+  //Finds the subreddit in the DB
+  //If the home subreddit was requested to be deleted returns a 500 error
+  Subreddits.findOne(query, (err, subFromDB) => {
+    if(err) {
+      console.log(err);
+    } else if (subFromDB.name === 'home') {
+      res.status(500).send('/r/home cannot be deleted');
+    } else {
+      //Finds all users subscribed to the subreddit
+      User.find({subscribedSubs: subFromDB.name}, (err, usersSubbed) => {
+        if(err) {
+          console.log(err);
+        } else {
+          //Unsubscribes each user that subscribes to the sub to be deleted
+          usersSubbed.forEach(userData => {
+            //Removes the subreddit from the subscribedSubs array
+            let subIndex = userData.subscribedSubs.indexOf(subFromDB.name);
+            userData.subscribedSubs.splice(subIndex, 1);
+
+            userData.save((err, updatedUser) => {
+              if(err) {
+                console.log(err);
+              } else {
+                console.log(userData.username + ' unsubscribed from ' + subFromDB.name);
+              }
+            });
+          });
+          
+          //Delete subreddit after unsubbing all the users
+          Subreddits.remove(query, err => {
+            if(err) {
+              console.log(err);
+            } else {
+              res.send('Success');
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
